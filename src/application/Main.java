c7fad0c6b4fe8190b684bed521adc5f27c0d598a
Vitor <vitorvvc2005@gmail.com>
2023-10-23 16:52:12 -0300

    Reescrita da classe main

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,11 @@ package application;
 
 import entities.*;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,9 +15,17 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("'Date: 'dd/MM/yyyy '& Hour: 'HH:mm");
+        ZoneId zoneId = ZoneId.systemDefault();
+        //ZoneId zoneId = ZoneId.of("America/New_York");
+        LocalDateTime localDateTime = LocalDateTime.now(zoneId);
+        System.out.println(formatter.format(localDateTime));
+
         // Exemplo com valores predefinidos sem usar arrayList
 
         Loja minhaLoja = new Loja(10);
+        System.out.println("=====================================================");
+        System.out.println("WITHOUT ARRAYLIST PRINTS: ");
 
         minhaLoja.adicionarProduto(new Computador("Computador A", 10000, 6, true));
         minhaLoja.adicionarProduto(new Computador("Computador B", 6000, 12, false));
@@ -30,23 +43,29 @@ public class Main {
 
 
         // Exemplo com valores predefinidos usando ArrayList
-        /*
-        LojaComArrayList loja = new LojaComArrayList();
-        ArrayList<LojaComArrayList> estoqueLoja = new ArrayList<>();
 
-        loja.adicionarItem(new Computador("Computador X", 17000, 12, true));
-        loja.adicionarItem(new Computador("Computador Y", 7000, 6, false));
+        LojaComArrayList loja = new LojaComArrayList();
+        System.out.println("=====================================================");
+        System.out.println("ARRAY LIST PRINTS: ");
+        loja.adicionarItem(new Computador("Computador X", 15000, 12, true));
+        loja.adicionarItem(new Computador("Computador Y", 5000, 6, false));
         loja.adicionarItem(new Celular("Celular X", 7000, 6, false));
         loja.adicionarItem(new Celular("Celular Y", 10000, 12, true));
 
-        loja.printList();
+        loja.elementosLoja();
+        loja.dadosProdutos();
         loja.removeItem(2);
         System.out.println("Quantidade de celulares com carregador: " + loja.qntCelCarregador());
+        System.out.println("Quantidade de PC'S com impressora: " + loja.qntPcImperssora());
         System.out.println("Computador mais barato da loja: " + loja.computadorBarato());
-         */
-        // Exemplo interagindo com o usuario ( SEM ARRAYLIST ) :(
+        System.out.println("Celular com menor tempo de garantia: " + loja.menorCelGarantia());
+        System.out.printf("Média dos precos da loja: %.2f%n", loja.mediaPreco());
 
-        Loja lojaUsuario = Util.obterDados(sc);
+
+        System.out.println("=====================================================");
+        // Exemplo interagindo com o usuario ( SEM ARRAYLIST ) :(
+        // System.out.println("GETTING DADS: ");
+        // Loja lojaUsuario = Util.obterDados(sc);
 
 
     }
